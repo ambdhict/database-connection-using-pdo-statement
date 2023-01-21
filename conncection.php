d5871e01494b2855cd730b691ef7b1df0d710a12
ambdhict <54847602+ambdhict@users.noreply.github.com>
2023-01-21 20:25:44 +0800

    code

---
+++ conncection.php
@@ -0,0 +1,10 @@
+<?php 
+  // Below codes is used to pdo statements
+	$dsn = "mysql:host=localhost;dbname=databasename";
+	$user = "root";
+	$passwd = "";
+
+	// $pdo = new PDO($dsn, $user, $passwd);
+
+	$pdo = new PDO($dsn, $user, $passwd);
+?>
